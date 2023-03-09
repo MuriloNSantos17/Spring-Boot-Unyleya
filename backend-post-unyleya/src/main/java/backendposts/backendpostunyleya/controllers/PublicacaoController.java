fce9dd626b5b78d0aa9adf32f9f3a66bb894d6de
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-03-09 20:39:15 -0300

    Commit route delete

--- backend-post-unyleya/src/main/java/backendposts/backendpostunyleya/controllers/PublicacaoController.java
+++ backend-post-unyleya/src/main/java/backendposts/backendpostunyleya/controllers/PublicacaoController.java
@@ -5,11 +5,15 @@ import backendposts.backendpostunyleya.exceptions.ResourceNotFoundException;
 import backendposts.backendpostunyleya.model.Publicacao;
 import backendposts.backendpostunyleya.repository.PublicacaoRepository;
 import backendposts.backendpostunyleya.service.SequenceGeneratorService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +38,22 @@ public class PublicacaoController {
         publicacao.setId(sequenceGeneratorService.generateSequence(Publicacao.SEQUENCE_NAME));
         return publicacaoRepository.save(publicacao);
     }
+    
+    @DeleteMapping("/publicacoes/{id}")
+    public Map<String,Boolean> deletePublicacao(@PathVariable (value="id") Long publicacaoId)
+    throws ResourceNotFoundException{
+        
+        Publicacao publicacao = publicacaoRepository.findById(publicacaoId)
+        .orElseThrow(() -> new ResourceNotFoundException("Publicação não encontrada!"));
+        
+        publicacaoRepository.delete(publicacao);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return response;
+    }
+            
+    
+    
     
     /*
         @GetMapping("/publicacoes/{id}")
