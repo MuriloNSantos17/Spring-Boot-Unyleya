01324f91b22e9c7bd2ad26ac612397eea6c8b170
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-03-14 20:08:22 -0300

    commit ajuste cors

--- unyleya/src/main/java/backendecommerce/unyleya/controllers/ProdutoController.java
+++ unyleya/src/main/java/backendecommerce/unyleya/controllers/ProdutoController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 public class ProdutoController {
     
